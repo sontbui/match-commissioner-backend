cf9af5dee503215141c0720763aa0d108da1c31c
son.bui <son.bui@opswat.com>
2025-09-29 19:36:22 +0700

    Start Project | Backend For Match Commissioner

---
+++ src/main/java/com/sontbui/match_commissioner/MatchCommissionerApplication.java
@@ -0,0 +1,13 @@
+package com.sontbui.match_commissioner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchCommissionerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchCommissionerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sontbui/match_commissioner/MatchCommissionerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sontbui.match_commissioner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchCommissionerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
