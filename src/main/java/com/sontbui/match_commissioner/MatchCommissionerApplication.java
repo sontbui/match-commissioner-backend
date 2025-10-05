df8c1dc2ed150b0fba9979314352dacf4483dc9b
son.bui <son.bui@opswat.com>
2025-10-05 20:31:36 +0700

    Fix main file | Load .env file for dev enviroment | Skip it if it on server

--- src/main/java/com/sontbui/match_commissioner/MatchCommissionerApplication.java
+++ src/main/java/com/sontbui/match_commissioner/MatchCommissionerApplication.java
@@ -9,13 +9,23 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class MatchCommissionerApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry -> 
-            System.setProperty(entry.getKey(), entry.getValue())
-        );
+		String env = System.getenv("SPRING_PROFILES_ACTIVE");
+		if (env == null || env.equalsIgnoreCase("dev")) {
+			try {
+				Dotenv dotenv = Dotenv.configure()
+						.directory("./")
+						.ignoreIfMissing()
+						.load();
+				dotenv.entries().forEach(
+						entry -> System.setProperty(entry.getKey(), entry.getValue()));
+				System.out.println("Loaded .env for local");
 
+			} catch (Exception e) {
+				System.out.println("Skipped .env loading. Using enviroment variable || Error message " + e.getMessage());
+			}
+		}
 
 		SpringApplication.run(MatchCommissionerApplication.class, args);
 	}
-
+	// docker run -p 8080:8080 --env-file .env match-commissioner-backend
 }
